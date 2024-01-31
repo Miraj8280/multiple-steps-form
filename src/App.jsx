931ceb18d85c257b5a1d63a27e200ffe1a19aad1
Miraj Asraf <mirajasraf786@gmail.com>
2024-01-31 14:53:48 +0530

    Created and designed stepper and stepper control components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import Stepper from "./components/Stepper";
+import StepperControl from "./components/StepperControl";
+
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-3xl text-blue-500 font-bold'>App</h1>
+    <div className="md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
+      {/* Stepper */}
+      <div className="container horizontal mt-5">
+        < Stepper />
+      </div>
+
+      {/* Navigation controls */}
+      < StepperControl />
     </div>
   );
 }
